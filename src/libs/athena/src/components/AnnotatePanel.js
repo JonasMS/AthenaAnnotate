a472c82e8d38ca6a180c76970ed98190ff0c34f2
JonasMS <jonas.sota@gmail.com>
2016-06-21 13:54:52 -0700

    FEAT - Make POST Request to api/create On SubmitBtn Click

--- src/libs/athena/src/components/AnnotatePanel.js
+++ src/libs/athena/src/components/AnnotatePanel.js
@@ -1,30 +1,63 @@
-import React from 'react';
 import FacebookLogout from './FacebookLogout';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import fetch from 'isomorphic-fetch';
+// import babel-polyfill
+require('es6-promise').polyfill();
 
-const AnnotatePanel = ({ logout, text }) => (
-  <div>
-    <h1> AnnotatePanel </h1>
-    <div>
+class AnnotatePanel extends Component {
+
+  submitHandler () {
+    const { annotation } = this.props;
+    // send annotation object to server
+    fetch('http://localhost:3000/api/create', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(annotation),
+    })
+    .then(res => {
+      if (res.status >= 400) {
+        throw new Error('bad response on create')
+      }
+    });
+  }
+
+  render () {
+    return (
       <div>
-        <FacebookLogout logout={logout} />
+        <div className="targetText">
+          <input
+            type="text"
+            value={this.props.exact}
+          />
+        </div>
+        <div className="bodyText">
+          <textarea />
+        </div>
+        <button
+          className="submitBtn"
+          onClick={() => this.submitHandler()}
+        >Submit</button>
+        <div>
+          <FacebookLogout logout={logout} />
+        </div>
       </div>
-      <div className="targetText">
-        <input
-          type="text"
-          value={text}
-        />
-      </div>
-      <div className="bodyText">
-        <textarea />
-      </div>
-    </div>
-  </div>
-);
+
+    );
+  }
+};
+
+const mapStateToProps = (state) => ({
+  annotation: state.annotation,
+  exact: state.annotation.target.exact,
+
+});
 
 AnnotatePanel.propTypes = {
-  logout: React.PropTypes.func.isRequired,
-  text: React.PropTypes.string.isRequired,
+  exact: React.PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
