5714ba9c4ce6b5b66f650a7bd390c2fa0e3769f6
JonasMS <jonas.sota@gmail.com>
2016-06-21 13:54:52 -0700

    FEAT - Make POST Request to api/create On SubmitBtn Click

--- src/libs/athena/src/components/AnnotatePanel.js
+++ src/libs/athena/src/components/AnnotatePanel.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import fetch from 'isomorphic-fetch';
+// import babel-polyfill
+require('es6-promise').polyfill();
 
-const AnnotatePanel = ({ text }) => (
-  <div>
-    <div className="targetText">
-      <input
-        type="text"
-        value={text}
-      />
-    </div>
-    <div className="bodyText">
-      <textarea />
-    </div>
-  </div>
-);
+class AnnotatePanel extends Component {
+
+  submitHandler () {
+    const { annotation } = this.props;
+    // send annotation object to server
+    fetch('http://localhost:3000/api/create', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(annotation),
+    })
+    .then(res => {
+      if (res.status >= 400) {
+        throw new Error('bad response on create')
+      }
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="targetText">
+          <input
+            type="text"
+            value={this.props.exact}
+          />
+        </div>
+        <div className="bodyText">
+          <textarea />
+        </div>
+        <button
+          className="submitBtn"
+          onClick={() => this.submitHandler()}
+        >Submit</button>
+      </div>
+
+    );
+  }
+};
 
 const mapStateToProps = (state) => ({
-  text: state.annotation.target.exact,
+  annotation: state.annotation,
+  exact: state.annotation.target.exact,
+
 });
 
 AnnotatePanel.propTypes = {
-  text: React.PropTypes.string.isRequired,
+  exact: React.PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(AnnotatePanel);
