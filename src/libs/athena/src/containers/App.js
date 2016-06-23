4e019d3d191c901e231e2ec93052b90a12e1eb4c
JonasMS <jonas.sota@gmail.com>
2016-06-22 19:46:20 -0700

    FEAT - Simple Insert span Tags

--- src/libs/athena/src/containers/App.js
+++ src/libs/athena/src/containers/App.js
@@ -3,13 +3,10 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import App from '../components/App';
 
+
 const mapStateToProps = (state) => ({
   user: state.user,
   panel: state.panel,
-  // annotation: state.annotation,
-  // annotations: state.annotations,
-  // widget: state.widget,
-  // adder: state.adder,
 });
 
 const mapDispatchToProps = (dispatch) => ({
