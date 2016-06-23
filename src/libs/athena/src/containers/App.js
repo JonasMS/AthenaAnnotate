ae2444f74ab1326064231dd84027b75263ed076d
JonasMS <jonas.sota@gmail.com>
2016-06-23 10:52:31 -0700

    DEBUG - Fix 'Highlight' Button

--- src/libs/athena/src/containers/App.js
+++ src/libs/athena/src/containers/App.js
@@ -49,7 +49,22 @@ class App extends Component {
     // listener for shortcut keys
     window
     .addEventListener('keydown', e => {
-      shortcutHandler(e, this.props);
+      const {
+        annotation,
+        annotations,
+        user,
+        widget,
+        actions,
+      } = this.props;
+      shortcutHandler(
+        e, {
+          annotation,
+          annotations,
+          user,
+          widget,
+          actions,
+        }
+      );
     });
   }
 
