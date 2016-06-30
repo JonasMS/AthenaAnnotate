303eda8e6fd59b7091e79e9eb61294383a513dd6
Elbert <elbert.lum@gmail.com>
2016-06-30 11:37:31 -0700

    (feat) adds images and titles to docs

--- src/client/App/Components/Doc.js
+++ src/client/App/Components/Doc.js
@@ -5,26 +5,47 @@ const changeLocation = (url) => {
   window.location = url;
 };
 
-const Doc = ({ url, id, count, onDocDelete, listView, filter }) => (
-  <li
-    className="card"
-  >
-    <div onClick={() => changeLocation(url)}>
-      {url}
-    </div>
-    {filter !== 'Self' ? null :
-      <button
-        className="waves-effect waves-light btn-floating"
-        onClick={onDocDelete}
-      >
-        <i className="material-icons">delete</i>
-      </button>}
-    {listView}
-    {listView ? <div>Annotations: {count}</div> : <VisibleAnnotationList id={id} />}
+const Doc = ({ url, title, image, id, count, onDocDelete, listView, filter }) => (
+  <li>
+    {listView
+      ?
+      <div className="media card">
+        <a onClick={() => changeLocation(url)}>
+          <div className="media-left">
+            <img className="media-object docimage small" src={image} alt="Thumbnail" />
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading">{title}</h4>
+            <p>{url}</p>
+            <span>Annotations: {count}</span>
+          </div>
+        </a>
+      </div>
+      :
+      <div className="card">
+        <a onClick={() => changeLocation(url)}>
+          <div className="image">
+            <img className="docimage large" src={image} alt="Document" />
+          </div>
+          <h4 className="heading">{title}</h4>
+          <p>{url}</p>
+        </a>
+        {filter !== 'Self' ? null :
+          <button
+            className="waves-effect waves-light btn-floating"
+            onClick={onDocDelete}
+          >
+            <i className="material-icons">delete</i>
+          </button>}
+        <VisibleAnnotationList id={id} />
+      </div>
+    }
   </li>
 );
 
 Doc.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
