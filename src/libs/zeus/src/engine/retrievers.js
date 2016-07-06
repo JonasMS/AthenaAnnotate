14e466bf54b72a1f44a8e8075ea4e63835f06cdb
JonasMS <jonas.sota@gmail.com>
2016-07-05 21:39:37 -0700

    DEBUG - Fix Bug Where Annotations With Multiple Nodes Would End With an Extra Char

---
+++ src/libs/zeus/src/engine/retrievers.js
@@ -0,0 +1,36 @@
+// returns a string docText that contains all
+// textNodes and an array of nodes that
+// correspond to each substring
+const parseDoc = doc => {
+  let docText = '';
+  let node;
+  const nodes = [];
+  const nodeIterator = document.createNodeIterator(
+    doc,
+    NodeFilter.SHOW_TEXT,
+    textNode => (
+        textNode.parentElement.nodeName.toLowerCase() !== 'script' ?
+            NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT
+      )
+    );
+  // iterate over each node
+  node = nodeIterator.nextNode();
+  while (!!node) {
+    // create node object
+    nodes.push(
+      {
+        textNode: node,
+        start: docText.length,
+      }
+    );
+    // concat to allText
+    docText += node.nodeValue;
+    node = nodeIterator.nextNode();
+  }
+  return {
+    docText,
+    nodes,
+  };
+};
+
+export default parseDoc;
