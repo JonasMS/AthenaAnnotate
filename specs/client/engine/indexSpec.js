db120b63a38b7a424c2358da6aa465762f0c493e
JonasMS <jonas.sota@gmail.com>
2016-07-05 21:38:26 -0700

    Feat - Add Spec Files

---
+++ specs/client/engine/indexSpec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { insertAnnote, retrieveAnnote } from '../../../src/libs/zeus/src/engine/';
+
+describe('Annotation Engine Methods', () => {
+  it('should be a function', () => {
+    expect(insertAnnote).to.be.a('function');
+  });
+});
