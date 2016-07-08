617ec7918d0fab208d0e8301010752d52b4d6773
David Valles <davidvalles@yahoo.com>
2016-07-07 21:01:41 -0700

    Make blacklist site code more robust.

--- src/libs/extension/src/index.js
+++ src/libs/extension/src/index.js
@@ -1,24 +1,37 @@
 import config from '../../../../config';
 
-var blacklist = ['www.facebook.com'];
+const blacklist = ['google', 'facebook'];
 
-if (blacklist.indexOf(document.location.hostname) !== -1) {
-  exit;
+const isBlackListedSite = function(site, list) {
+  for (let i = 0; i < list.length; i ++) {
+    const re = new RegExp(list[i], 'g');
+    if (site.search(re) !== -1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const loadApp = function() {
+  const baseUrl = process.env.NODE_ENV === 'production'
+                ? config.url.host
+                : `${config.url.host}:${config.url.port}`;
+
+  const req = new XMLHttpRequest();
+
+  req.open('GET', `${baseUrl}/zeus/zeus.js`);
+  req.onload = function() {
+    const script = document.createElement('script');
+    script.textContent = req.responseText;
+    (document.head || document.documentElement).appendChild(script);
+  };
+  req.onerror = function() {
+    console.log('AthenaAnnotate extension cannot fetch zeus!');
+  };
+  req.send();
+};
+
+
+if (!isBlackListedSite(document.location.hostname, blacklist)) {
+  loadApp();
 }
-
-const baseUrl = process.env.NODE_ENV === 'production'
-              ? config.url.host
-              : `${config.url.host}:${config.url.port}`;
-
-const req = new XMLHttpRequest();
-
-req.open('GET', `${baseUrl}/zeus/zeus.js`);
-req.onload = function() {
-  const script = document.createElement('script');
-  script.textContent = req.responseText;
-  (document.head || document.documentElement).appendChild(script);
-};
-req.onerror = function() {
-  console.log('AthenaAnnotate cannot load zeus!');
-};
-req.send();
