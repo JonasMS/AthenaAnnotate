dbaf6b81f81a5bc8774cdbe615e1197c7a6b6505
JonasMS <jonas.sota@gmail.com>
2016-07-30 14:21:25 -0700

    FEAT - Dev Basic Testing Structure

--- specs/client/engine/indexSpec.js
+++ specs/client/engine/indexSpec.js
@@ -7,28 +7,75 @@ import NodeFilter from '../../componentMocks/NodeFilter';
 import parseDoc from '../../../src/libs/zeus/src/engine/./retrievers';
 import { locateAnnote } from '../../../src/libs/zeus/src/engine/./processors';
 import { createRange  } from '../../../src/libs/zeus/src/engine/./utils';
+import { annote01 } from './specAnnotes';
 // import { HTMLElement } from '../../componentMocks/HTMLElement';
 // import { retrieveAnnote } from '../../../src/libs/zeus/src/engine/';
 
-
-const filePath = path.join(__dirname, '/../../../src/server/views/spec/basic.html');
-const htmlDoc = fs.readFileSync(filePath, 'utf8');
-global.document = jsdom.jsdom(htmlDoc);
-const window = document.defaultView;
-global.NodeFilter = NodeFilter;
-// global.HTMLElement = window.HTMLElement;
-// console.log(HTMLElement);
+let filePath;
+let htmlDoc;
+let window;
+let targetStartNode;
+let targetEndNode;
 
 describe('Annotation Engine Methods', () => {
-  describe('parseDoc method', () => {
-    it('parseDoc should be a function', () => {
-      expect(parseDoc).to.be.a('function');
+    // TODO: before each clear variable values
+  it('parseDoc should be a function', () => {
+    expect(parseDoc).to.be.a('function');
+  });
+
+  describe('Test Basic DOM Structure', () => {
+    filePath = path.join(__dirname, '/../../../src/server/views/spec/basic.html');
+    htmlDoc = fs.readFileSync(filePath, 'utf8');
+    global.document = jsdom.jsdom(htmlDoc);
+    window = document.defaultView;
+    global.NodeFilter = NodeFilter;
+    const { docText, nodes } = parseDoc(document);
+
+    it('docText should be a string', () => { // TODO: test beforeEach
+      expect(docText).to.be.a('string');
     });
-    it('parseDoc should return the correct docText and nodes', () => {
-      const { docText, nodes } = parseDoc(document);
-      console.log(nodes);
+
+    it('nodes should be an array', () => { // TODO: test beforeEach
       expect(nodes).to.be.a('array');
-      // expect(document).to.be.a('object');
+    });
+
+    describe('Test annote01', () => {
+      it('should return an object', () => {
+        expect(locateAnnote(annote01, docText, nodes)).to.be.a('object');
+      });
+
+      it('should return the correct range', () => {
+      });
+
+      it('should return the correct parent elements', () => {
+
+      });
+    });
+
+    describe('Test annote02', () => {
+      it('should return an object', () => {
+        expect(locateAnnote(annote01, docText, nodes)).to.be.a('object');
+      });
+
+      it('should return the correct range', () => {
+      });
+
+      it('should return the correct parent elements', () => {
+
+      });
+    });
+
+    describe('Test annote03', () => {
+      it('should return an object', () => {
+        expect(locateAnnote(annote01, docText, nodes)).to.be.a('object');
+      });
+
+      it('should return the correct range', () => {
+      });
+
+      it('should return the correct parent elements', () => {
+
+      });
     });
   });
 });
